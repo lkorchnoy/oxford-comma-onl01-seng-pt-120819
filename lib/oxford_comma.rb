49bb9406a05923f244681dbbf446a3989d68291f
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-17 17:16:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  array = ["fiddleheads","okra","kohlrabi"]
+  array.join(" , ")
+  array.join = new_array
+  return new_array
 end
+
+def oxford_comma(array)
+  array.join(" , ")
+  array.join = new_array
+  return new_array
+end
+  
+oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
+
+
+
+
