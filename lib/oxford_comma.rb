5744127f46014851d026a30a8574554b4d254b17
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-17 17:23:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
+  array = ["fiddleheads","okra","kohlrabi"]
+  array.join(" , ")
+  array.join = new_array
+  return new_array
+end
+
+def oxford_comma(array)
+  array.join(" , ")
+  array.join = new_array
+  return new_array
+end
+  
+oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
+
+def oxford_comma(array)
+string = []
+while oxford_comma > 3 do
+  string << "array.join(" , ")"
 
 end
+oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])
+end
+
